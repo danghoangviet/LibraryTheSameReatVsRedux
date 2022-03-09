5e3c3a4dc29aafad7573fafd01086a0b55e193e0
HoangViet <danghoangviet2000@gmail.com>
2022-03-09 23:20:09 +0700

    Source new library the same redux,reat

---
+++ component/Footer.js
@@ -0,0 +1,21 @@
+import html from "../core.js";
+import {connect} from "../store.js"
+function TodoList({todos,filter,filters}) {
+    return html`
+    <footer class="footer">
+        <span class="todo-count"><strong>${todos.filter(filters.active).length}</strong> item left</span> 
+        <ul class="filters">
+            ${Object.keys(filters).map(type => html`<li>
+            <a 
+            onclick ="dispatch('filterStatus','${type}')"
+            class="${filter === type && 'selected'}" href="#">${type[0].toUpperCase() + type.slice(1)}</a>
+        </li>` )}
+            
+        </ul>
+        ${todos.filter(filters.completed).length > 0 && html`
+        <button onclick="dispatch('clearCompleted')" class="clear-completed">Clear completed</button>
+        `}
+    </footer>
+    `
+}
+export default connect()(TodoList);
---
+++ logger.js
@@ -0,0 +1,11 @@
+export default function Logger(reducer){
+    return (prevState, action, args) => {
+        console.group(action)
+        console.log('Prev State:' , prevState)
+        console.log('Arguments action', args)
+        const nextState = reducer(prevState, action, args)
+        console.log('Next State:' , nextState)
+        console.groupEnd
+        return nextState
+    }
+}
